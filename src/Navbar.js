0089e95b31d8e73ad50f75870b48b3753f3af464
Hazel Wu <tinazx056@gmail.com>
2022-09-10 23:40:30 +0800

    feat: Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Box, Button, Flex, Image, Link, Spacer } from '@chakra-ui/react'
+import Facebook from './assets/social-media-icons/facebook_32x32.png'
+import Twitter from './assets/social-media-icons/twitter_32x32.png'
+import Email from './assets/social-media-icons/email_32x32.png'
 
 const Navbar = ({ accounts, setAccounts }) => {
   const isConnected = Boolean(accounts[0]);
@@ -15,24 +19,55 @@ const Navbar = ({ accounts, setAccounts }) => {
   }
 
   return (
-    <div>
-      {/* Left ide - Social Media Icons */}
-      <div>Facebook</div>
-      <div>Twitter</div>
-      <div>Email</div>
+    <Flex justify="space-between" align="center" padding="30px">
+      {/* Left Side - Social Media Icons */}
+
+      <Flex justify="space-around" width="40%" padding="0 75px">
+        <Link href="https://www.facebook.com">
+          <Image src={Facebook} boxSize="42px" margin="0 15px" />
+        </Link>
+        <Link href="https://www.twitter.com">
+          <Image src={Twitter} boxSize="42px" margin="0 15px" />
+        </Link>
+        <Link href="https://www.gamil.com">
+          <Image src={Email} boxSize="42px" margin="0 15px" />
+        </Link>
+      </Flex>
 
       {/* Right Side - Section and Connect */}
-      <div>About</div>
-      <div>Mint</div>
-      <div>Team</div>
+      <Flex
+        justify="space-around"
+        align="center"
+        width="40%"
+        padding="30px 30px 30px 30px"
+      >
+        <Box margin="0 15px">About</Box>
+        <Spacer />
+        <Box margin="0 15px">Mint</Box>
+        <Spacer />
+        <Box margin="0 15px">Team</Box>
+        <Spacer />
 
-      {/* Connect */}
-      {isConnected ? (
-        <p>Connected</p>
-      ) : (
-        <button onClick={connectAccount}>連接錢包</button>
-      )}
-    </div>
+        {/* Connect */}
+        {isConnected ? (
+          <Box margin="0 15px">Connected</Box>
+        ) : (
+          <Button
+            backgroundColor="#D6517D"
+            borderRadius="5px"
+            boxShadow="0px 2px 2px 1px #0F0F0F"
+            color="white"
+            cursor="pointer"
+            fontFamily="inherit"
+            padding="15px"
+            margin="0 15px"
+            onClick={connectAccount}
+          >
+            Connect
+          </Button>
+        )}
+      </Flex>
+    </Flex>
   )
 }
 
