0bb4af7daa33d4b5c47339c224c1ce71cfe64cf1
Hazel Wu <tinazx056@gmail.com>
2022-09-11 01:02:18 +0800

    feat: set ChakraProvider

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import { useState } from 'react'
 import './App.css';
 import MainMint from './MainMint';
 import Navbar from './Navbar';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   const [accounts, setAccounts] = useState([]);
 
   return (
-    <div className="overlay">
-      <div className="App">
-        <Navbar accounts={accounts} setAccounts={setAccounts} />
-        <MainMint accounts={accounts} setAccounts={setAccounts} />
-      </div>
+    <ChakraProvider>
+      <div className="overlay">
+        <div className="App">
+          <Navbar accounts={accounts} setAccounts={setAccounts} />
+          <MainMint accounts={accounts} setAccounts={setAccounts} />
+        </div>
 
-      <div className="moving-background"></div>
-    </div>
+        <div className="moving-background"></div>
+      </div>
+    </ChakraProvider>
   );
 }
 
 export default App;
-// 0x5A5E65f915b8bCC6d856FEF6ed81f895062882d1
