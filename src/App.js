b4781b567f44c1725c8c05faf951ec270ebb4d27
Hazel Wu <tinazx056@gmail.com>
2022-09-10 23:29:04 +0800

    style: set app basic style

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ function App() {
   const [accounts, setAccounts] = useState([]);
 
   return (
-    <div className="App">
-      <Navbar accounts={accounts} setAccounts={setAccounts} />
-      <MainMint accounts={accounts} setAccounts={setAccounts} />
+    <div className="overlay">
+      <div className="App">
+        <Navbar accounts={accounts} setAccounts={setAccounts} />
+        <MainMint accounts={accounts} setAccounts={setAccounts} />
+      </div>
+
+      <div className="moving-background"></div>
     </div>
   );
 }
